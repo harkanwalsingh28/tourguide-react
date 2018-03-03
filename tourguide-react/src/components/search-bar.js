9149a4e5cdab9be0b81c1c67660cf4802aabf68b
harkanwalsingh28 <harkanwalsingh28@gmail.com>
2018-03-04 00:11:20 +0530

    button inserted

--- tourguide-react/src/components/search-bar.js
+++ tourguide-react/src/components/search-bar.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
+import TourList from './tourlist';
 
 class SearchBar extends Component{
     constructor(props){
         super(props);
         this.state={term:''};
     }
-    onInputChange(event){
-        this.setState(term=>{event.target.value});
+    handleChange(event){
+        this.setState({term:event.target.value});
     }
     render() {
         
         
         return(
             <div>
-                <input value={this.state.term}
-                onChange={this.onInputChange} />
+                <input type="text" value={this.state.term}
+                onChange={this.handleChange} />
+                <button onClick={()=>this.touritems(this.state.term)} >Click!</button>
                 </div>
         );
     }
